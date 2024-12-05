aac6b304c146432e95097c30f741929ab9b1076c
gfxv <superfreezu@gmail.com>
2024-12-05 22:10:46 +0300

    chore: remove redundant prompt value

--- internal/model/gemini_model.go
+++ internal/model/gemini_model.go
@@ -7,21 +7,6 @@ import (
 	"google.golang.org/api/option"
 )
 
-const PROMPT = `
-Create well-formed git commit message based of off the currently staged file contents. 
-The message should convey why something was changed and not what changed. 
-Use the well known format that has the prefix chore, fix, etc.
-
-Only include changes to source files for the programming languages, shell configurations files, 
-documentation such as readme and other .mds, and any changes to package management file. 
-Exclude any lock or sum files.
-
-Do not use markdown format for the output.
-
-For the first line of the commit message, this must be constrained to 40 characters as a 
-maximum and use additional lines for any further context.
-`
-
 type GeminiModel struct {
 	apiKey string
 	model  string
