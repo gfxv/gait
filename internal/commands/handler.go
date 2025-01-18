5a9eaa2cf3e0ddc548859d35e464b231c6e24b52
gfxv <superfreezu@gmail.com>
2025-01-18 17:44:57 +0300

    docs: improve cli app usage message
    update the usage message to be more informative and
    remove the outdated instructions related to commit
    message generation.

--- internal/commands/handler.go
+++ internal/commands/handler.go
@@ -10,10 +10,10 @@ func NewHandler(commands ...*cli.Command) *cli.App {
 		EnableBashCompletion: true,
 
 		Name:  "gait",
-		Usage: "Use 'gait c' or 'gait commit' to generate commit messsage based on staged files",
+		Usage: "An AI powered tool for generating commit messages",
 		Action: func(*cli.Context) error {
-			// TODO: print proper help
-			fmt.Println("Use 'gait c' or 'gait commit' to generate commit messsage based on staged files")
+			// TODO: print proper help (probably add some info like description, version, etc.)
+			fmt.Println("Type 'gait help' to see all available commands")
 			return nil
 		},
 		Commands: commands,
